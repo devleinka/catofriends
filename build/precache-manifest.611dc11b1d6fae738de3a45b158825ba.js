0c7aaeaf37ab74eab21d4aaba038082602804751
devleinka <42700247+devleinka@users.noreply.github.com>
2020-09-01 13:01:55 +0200

    update

---
+++ build/precache-manifest.611dc11b1d6fae738de3a45b158825ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfc77074c94dba9f58fbfc9f445c8d9",
+    "url": "/reduxfriends/index.html"
+  },
+  {
+    "revision": "d5eea97eae9e0e979615",
+    "url": "/reduxfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "61d485b6b93b38ac60c7",
+    "url": "/reduxfriends/static/css/main.c30cfbfd.chunk.css"
+  },
+  {
+    "revision": "d5eea97eae9e0e979615",
+    "url": "/reduxfriends/static/js/2.8462ca26.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/reduxfriends/static/js/2.8462ca26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61d485b6b93b38ac60c7",
+    "url": "/reduxfriends/static/js/main.72bdea5b.chunk.js"
+  },
+  {
+    "revision": "430264edaceb5481e349",
+    "url": "/reduxfriends/static/js/runtime-main.46b71f38.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/reduxfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
